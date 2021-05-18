36b107a3bc7e95ba6daf838998f0505274b4c967
Gabija Grazenaite <grgabija@gmail.com>
2021-05-18 10:36:11 +0300

    Initial Commit

---
+++ doggallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  doggallery
+//
+//  Created by Gabija on 2021-05-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
