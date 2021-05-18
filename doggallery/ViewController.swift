c5974cf552b49001444a2c8defa818c405e47f35
Gabija Grazenaite <grgabija@gmail.com>
2021-05-18 18:37:40 +0300

    Initial commit

--- doggallery/ViewController.swift
+++ doggallery/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    // MARK: - Outlets
+    @IBOutlet weak var galleryTitle: UILabel!
+    
+    // MARK: - Methods
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
