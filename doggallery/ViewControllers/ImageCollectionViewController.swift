bcec84c155b73a3e8f9a52c750c5d44704aa6eab
Gabija Grazenaite <grgabija@gmail.com>
2021-05-20 17:03:58 +0300

    Added ImagePreview and navigation to it

--- doggallery/ViewControllers/ImageCollectionViewController.swift
+++ doggallery/ViewControllers/ImageCollectionViewController.swift
@@ -9,39 +9,51 @@ import UIKit
 
 class ImageCollectionViewController: UICollectionViewController {
     
+    // MARK: - Declarations
+    var selectedImage: UIImage!
+    
     struct Storyboard {
         static let imageCell = "ImageCollectionViewCell"
+        static let imagePreviewControllerSegue = "ImagePreview"
         
         static let horizontalPaddings: CGFloat = 2.0
         static let numberItemsPerRow: CGFloat = 3.0
     }
     
     // MARK: - Methods
-    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         guard let collectionViewWidth = collectionView?.frame.width else {
             return
         }
-        let imageWidth = (collectionViewWidth - Storyboard.horizontalPaddings) / (Storyboard.numberItemsPerRow)
         
+        let imageWidth = (collectionViewWidth - Storyboard.horizontalPaddings) / (Storyboard.numberItemsPerRow)
         let layout = collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: imageWidth, height: imageWidth)
-        // Do any additional setup after loading the view.
-    }
-    
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 40
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.imageCell, for: indexPath) as! ImageCollectionViewCell
+        
         return cell
     }
     
+    //MARK: - UICollectionViewDelegate
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedImage = UIImage(named: "paw")
+        performSegue(withIdentifier: Storyboard.imagePreviewControllerSegue, sender: selectedImage)
+    }
+    
+    //MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Storyboard.imagePreviewControllerSegue {
+            let imagePreviewViewController = segue.destination as! ImagePreviewViewController
+            imagePreviewViewController.image = selectedImage
+        }
+    }
 }
---
+++ doggallery/ViewControllers/ImagePreviewViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ImagePreviewViewController.swift
+//  doggallery
+//
+//  Created by Gabija on 2021-05-20.
+//
+
+import UIKit
+
+class ImagePreviewViewController: UIViewController {
+    
+    @IBOutlet weak var imagePreview: UIImageView!
+    var image: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePreview.image = image
+    }
+
+    
+    
+}
